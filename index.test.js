b1588beb8ff113f4e694bfb025b7cdf0b0bec36d
J-K. Solbakken <jksolbakken@gmail.com>
2021-11-27 21:25:21 +0100

    a bit more thorough testing

--- index.test.js
+++ index.test.js
@@ -1,13 +1,21 @@
 const filter = require('./index')
 
-describe("only query params from the no-no list are removed", () => {
+describe("params from the no-no list are removed", () => {
    ['utm_whatever', 'fbclid', 'oly_whatever', '_openstat', 'mkt_tok'].forEach((param) => {
       const orig = new URL(`https://example.com?${param}=yourprivacyarebelongtous&something=whatever`)
       test(`${param} is bad`, () => {
          const filtered = new URL(filter.removeTrackingParams(orig.toString()))
-         expect(filtered).not.toBe(orig)
+         expect(filtered.searchParams.get('fbclid')).toBeNull()
          expect(filtered.searchParams.get('something')).toBe('whatever')
       })
+   })
+
+   const orig = new URL(`https://example.com?fbclid=evil&utm_source=bad&something=whatever`)
+   test(`multiple no-no params are also bad`, () => {
+      const filtered = new URL(filter.removeTrackingParams(orig.toString()))
+      expect(filtered.searchParams.get('fbclid')).toBeNull()
+      expect(filtered.searchParams.get('utm_source')).toBeNull()
+      expect(filtered.searchParams.get('something')).toBe('whatever')
    })
 
    test('url with no query params is left untouched', () => {
